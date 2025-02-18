e51335e1b787d192d0e6a903c3bf0683701f2f3b
Alex Fabre <alex.fabre@rtone.fr>
2025-02-18 08:09:23 +0100

    cmake: linker: use defined 'set_linker_property'
    Replace the default 'set_property'.
    TARGET linker is set by default by 'set_linker_property'

--- cmake/linker/ld/clang/linker_flags.cmake
+++ cmake/linker/ld/clang/linker_flags.cmake
@@ -1,15 +1,15 @@
 # The coverage linker flag is specific for clang.
 if (CONFIG_COVERAGE_NATIVE_GCOV)
-  set_property(TARGET linker PROPERTY coverage --coverage)
+  set_linker_property(PROPERTY coverage --coverage)
 elseif(CONFIG_COVERAGE_NATIVE_SOURCE)
-  set_property(TARGET linker PROPERTY coverage -fprofile-instr-generate -fcoverage-mapping)
+  set_linker_property(PROPERTY coverage -fprofile-instr-generate -fcoverage-mapping)
 endif()
 
 # Extra warnings options for twister run
-set_property(TARGET linker PROPERTY ld_extra_warning_options ${LINKERFLAGPREFIX},--fatal-warnings)
+set_linker_property(PROPERTY ld_extra_warning_options ${LINKERFLAGPREFIX},--fatal-warnings)
 
 # GNU ld and LLVM lld complains when used with llvm/clang:
 #   error: section: init_array is not contiguous with other relro sections
 #
 # So do not create RELRO program header.
-set_property(TARGET linker APPEND PROPERTY cpp_base ${LINKERFLAGPREFIX},-z,norelro)
+set_linker_property(APPEND PROPERTY cpp_base ${LINKERFLAGPREFIX},-z,norelro)
